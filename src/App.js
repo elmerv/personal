7c18c2fac83abf9ab7365de5b842c6ea73eda64f
elmerv <vasquezelmer101@gmail.com>
2020-10-01 01:33:52 -0700

    Animation is still missing. Footer is 80% complete. The highlight button needs to be added. The gif needs to be added. A final comparions of wireframe and current website is needed to determine missing css properties.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import GetHighlights from './components/getHighlights'
 import MainPage from './components/mainPage'
-import "normalize.css/normalize.css";
 function App() {
   return(
     <div className = "App">
