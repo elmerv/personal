0856faec142e474ca0b0a749299ecac66e385dd2
elmerv <elfvasqu@ucsc.edu>
2020-12-30 23:49:48 -0800

    Updates

---
+++ precache-manifest.21343a3dba9321e7e6ac65ab9056e92a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2023101e27ab8032d00108d07f6f357b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74e54ee7a714060e4c59",
+    "url": "/static/css/main.06ed076d.chunk.css"
+  },
+  {
+    "revision": "c8d8ad5373585992eb54",
+    "url": "/static/js/2.4ef613bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ef613bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e54ee7a714060e4c59",
+    "url": "/static/js/main.14762b72.chunk.js"
+  },
+  {
+    "revision": "96bb48dc442d17975035",
+    "url": "/static/js/runtime-main.b19fe365.js"
+  },
+  {
+    "revision": "69306d414ab4b6d312f9640a1ac73537",
+    "url": "/static/media/resume.69306d41.pdf"
+  },
+  {
+    "revision": "54f638ee354dfd01a821cfd7e3d6bebf",
+    "url": "/static/media/typingGif.54f638ee.gif"
+  }
+]);
