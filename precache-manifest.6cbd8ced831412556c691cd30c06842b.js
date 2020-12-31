1a7bd6c84805f72886ddc809ea8727d90ad4d015
elmerv <elfvasqu@ucsc.edu>
2020-12-30 23:56:08 -0800

    Updates

---
+++ precache-manifest.6cbd8ced831412556c691cd30c06842b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f1ebba686bdce2adda5af8f0a8b47a",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "74e54ee7a714060e4c59",
+    "url": "/personal/static/css/main.06ed076d.chunk.css"
+  },
+  {
+    "revision": "c8d8ad5373585992eb54",
+    "url": "/personal/static/js/2.4ef613bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal/static/js/2.4ef613bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e54ee7a714060e4c59",
+    "url": "/personal/static/js/main.14762b72.chunk.js"
+  },
+  {
+    "revision": "79bd088f050178a8e191",
+    "url": "/personal/static/js/runtime-main.8009a80f.js"
+  },
+  {
+    "revision": "69306d414ab4b6d312f9640a1ac73537",
+    "url": "/personal/static/media/resume.69306d41.pdf"
+  },
+  {
+    "revision": "54f638ee354dfd01a821cfd7e3d6bebf",
+    "url": "/personal/static/media/typingGif.54f638ee.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21343a3dba9321e7e6ac65ab9056e92a.js"
+  "/personal/precache-manifest.6cbd8ced831412556c691cd30c06842b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
